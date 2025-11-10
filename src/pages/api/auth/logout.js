02ed38513918d850f5035b0b293556506137105e
AlejandroJ07 <buitragojherson22@gmail.com>
2025-11-09 23:27:47 -0500

    fix: remove API routes

--- src/pages/api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "../../../lib/supabase";
-
-export const POST = async (context) => {
-  const supabase = createClient(context);
-  await supabase.auth.signOut();
-  return context.redirect("/login");
-};
