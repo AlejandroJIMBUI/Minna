44e26b8f27c70c2d46d0ff21dc3c4b0e2ec44b59
AlejandroJ07 <buitragojherson22@gmail.com>
2025-11-16 19:25:54 -0500

    resolve: merge conflicts with main (adapater installed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: '@astrojs/node',
   site: 'https://alejandrojimbui.github.io',
   base: '/Minna/',
 });
